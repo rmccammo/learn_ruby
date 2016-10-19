1ae1ffb2a38e4798fc9e1cdbed35c81cf6e7dcd2
Robert <rlmccammon@live.com>
2016-10-19 18:13:26 -0500

    Completed 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
 #write your code here
+def echo(phrase)
+	return phrase
+end
+
+def shout(phrase)
+	return phrase.upcase
+end
+
+def repeat(phrase, times = 2)
+	new_phrase = ""
+	(0..times - 1).each do 
+		new_phrase += phrase + " "
+	end
+
+	return new_phrase.rstrip #Return string with last space removed
+	#return phrase + " " + phrase
+end
+
+def start_of_word(word, letters)
+	return word[0..letters - 1]
+end
+
+def first_word(phrase)
+	return phrase.split[0]
+end
+
+def titleize(title)
+	words = title.split
+	new_title = ""
+
+	words.each_with_index do |current_word, index|
+		if(current_word != "and" && current_word != "the" && current_word != "over" || index == 0)
+			current_word = current_word.capitalize
+		end
+
+		new_title += current_word + " "
+	end
+
+	return new_title.rstrip
+end
