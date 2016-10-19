a909ddec3d8b411e6c44cfd6852f95c19ce18764
Robert <rlmccammon@live.com>
2016-10-19 13:58:53 -0500

    Completed 02_calculator with extra credit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,51 @@
 #write your code here
+
+def add(first_num, second_num)
+	return first_num + second_num
+end
+
+def subtract(first_num, second_num)
+	return first_num - second_num
+end
+
+def sum(array)
+	sum = 0
+	array.each do |i|
+		sum += i
+	end
+
+	return sum
+end
+
+def multiply(array)
+		sum = 1
+	array.each do |i|
+		sum *= i
+	end
+
+	return sum
+end
+
+def power(base, exponent)
+	result = 1
+
+	(0..exponent - 1).each do
+		result *= base
+	end
+
+	return result
+end
+
+def factorial(value)
+	if value == 0
+		return 1
+	end
+
+	result = 1
+
+	(1..value).each do |i|
+		result *= i
+	end
+
+	return result
+end
