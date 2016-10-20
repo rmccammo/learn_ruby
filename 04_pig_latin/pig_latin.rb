d42128eba4977e6a071e0114d95c7e3b7f1d3d8a
Robert <rlmccammon@live.com>
2016-10-20 07:22:41 -0500

    Completed first two tests for 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+def translate(phrase)
+	first_letter = phrase[0]
+	vowels = "aeiou"
+
+	#If first letter is a vowel, return the word with "ay" at the end
+	if vowels.include? first_letter
+		return phrase + "ay"
+	#Otherwise, move the first letter to the end, then add "ay"
+	else
+		return phrase[1..phrase.length] + first_letter + "ay"
+	end 
+end
