7a32cb4d8641e7f59ffb8e4a3d1d3d03a3b3a994
Robert <rlmccammon@live.com>
2016-10-20 17:31:24 -0500

    Completed 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -9,5 +9,22 @@ class Book
 	end
 
 	def title=(new_title)
+		@title = titleize(new_title)
+	end
+
+	def titleize(title)
+		words = title.split
+		new_title = ""
+		lower_case_words = ["and", "the", "over", "in", "of", "a", "an"] #Words that should stay lower case
+
+		words.each_with_index do |current_word, index|
+			if(not lower_case_words.include? current_word or index == 0)
+				current_word = current_word.capitalize
+			end
+
+			new_title += current_word + " "
+		end
+
+		return new_title.rstrip
 	end
 end
