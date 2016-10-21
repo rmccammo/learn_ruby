031e1e108515b7f512a209ba37783bb8c1810b15
Robert <rlmccammon@live.com>
2016-10-21 07:24:16 -0500

    Added getters and setters for 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
   #write your code here
+  def initialize()
+  	@seconds = 0 #Total seconds to time
+  end
+
+  def seconds
+  	return @seconds
+  end
+
+  def seconds=(new_seconds)
+  	@seconds = new_seconds
+  end
 end
