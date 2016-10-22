73c0893b01c667b24b82b25e316f7256f6ec60cf
Robert <rlmccammon@live.com>
2016-10-22 07:57:11 -0500

    Completed 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -2,6 +2,8 @@ class Timer
   #write your code here
   def initialize()
   	@seconds = 0 #Total seconds to time
+  	@minutes = 0
+  	@hours = 0
   end
 
   def seconds
@@ -11,4 +13,25 @@ class Timer
   def seconds=(new_seconds)
   	@seconds = new_seconds
   end
+
+  def padded(number)
+  	if(number < 10) then
+  		return "0" + number.to_s
+  	else
+  		return number.to_s
+  	end
+  end
+
+  def time_string()
+  	#Convert seconds to minutes
+  	@minutes = @seconds / 60
+  	@seconds = @seconds % 60
+
+  	#Convert minutes to hours
+  	@hours = @minutes / 60
+  	@minutes = @minutes % 60
+
+  	return padded(@hours) + ":" + padded(@minutes) + ":" + padded(@seconds)
+  end
+
 end
--- 06_timer/timer_spec.rb
+++ 06_timer/timer_spec.rb
@@ -18,6 +18,23 @@ describe "Timer" do
     expect(@timer.seconds).to eq(0)
   end
 
+
+  # One way to implement the Timer is with a helper method.
+  # Uncomment these specs if you want to test-drive that
+  # method, then call that method from inside of time_string.
+  
+   describe 'padded' do
+     it 'pads zero' do
+       expect(@timer.padded(0)).to eq('00')
+     end
+     it 'pads one' do
+       expect(@timer.padded(1)).to eq('01')
+     end
+     it "doesn't pad a two-digit number" do
+       expect(@timer.padded(12)).to eq('12')
+     end
+   end
+
   describe 'time_string' do
     it "should display 0 seconds as 00:00:00" do
       @timer.seconds = 0
@@ -41,20 +58,6 @@ describe "Timer" do
   end
 
 
-  # One way to implement the Timer is with a helper method.
-  # Uncomment these specs if you want to test-drive that
-  # method, then call that method from inside of time_string.
-  #
-  # describe 'padded' do
-  #   it 'pads zero' do
-  #     expect(@timer.padded(0)).to eq('00')
-  #   end
-  #   it 'pads one' do
-  #     expect(@timer.padded(1)).to eq('01')
-  #   end
-  #   it "doesn't pad a two-digit number" do
-  #     expect(@timer.padded(12)).to eq('12')
-  #   end
-  # end
+
 
 end
